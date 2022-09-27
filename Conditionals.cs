65786d85225bfcd3625ba3910b7fa62aaada72a2
brittsaundrs <10976300@uvu.edu>
2022-09-26 23:51:44 -0600

    Activity-2
    I know how to do this in Python better, I just need to find a way to translate all that knowledge to C#.

---
+++ Conditionals.cs
@@ -0,0 +1,18 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+		int x = 45;
+		int y = 765;
+		if (x < y )
+		{
+			Console.WriteLine("This worked!");
+		}
+        else
+        {
+            Console.WriteLine("I guess not...")
+        }
+	}
+}
---
+++ Switch.cs
@@ -0,0 +1,24 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+		int year = 2;
+		switch (year)
+		{
+			case 1:
+				Console.WriteLine("Freshman");
+				break;
+			case 2:
+				Console.WriteLine("Sophomore");
+				break;
+			case 3:
+				Console.WriteLine("Junior");
+				break;
+			case 4:
+				Console.WriteLine("Senior");
+				break;
+		}
+	}
+}
