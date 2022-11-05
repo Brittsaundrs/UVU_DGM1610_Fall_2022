aac66fb44e555d145a4497face945e9aa77752a7
brittsaundrs <10976300@uvu.edu>
2022-11-05 13:44:34 -0600

    Unit 4 Challenge -- Space Invaders
    Working on recreating space invaders in 3D

---
+++ Space Invaders Reinvented/Assets/Scripts/FloatData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+// notes how many enemies in game
+
+public class FloatData : ScriptableObject
+{
+    public int numberOfEnemies;
+}
