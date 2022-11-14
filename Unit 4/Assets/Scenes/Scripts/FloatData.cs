fdf9425925f55ad52ab2d05cd7faae05b0c93862
brittsaundrs <10976300@uvu.edu>
2022-11-14 14:17:30 -0700

    Unit 5 -- Intro to Unity Events 02
    Watch Introduction to UnityEvents
    -----
    3/3 videos

--- Unit 4/Assets/Scenes/Scripts/FloatData.cs
+++ Unit 4/Assets/Scenes/Scripts/FloatData.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class FloatData : ScriptableObject
 {
     public float value;
+
+    public void SetValue(float num)
+    {
+        value = num;
+    }
+
     public void UpdateValue(float num)
     {
         value  += num;
