5f6bb54567fa951125d90b1fb8064b2fa18cf427
brittsaundrs <10976300@uvu.edu>
2022-11-22 19:11:30 -0700

    Unit 6a -- Project Setup Completed

---
+++ UFO Defence Force Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// namespaces & libraries
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set horizontalInput to receive values from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+    	    // gets a key binding, better than GetKey
+        transform.Translate(Vector3.right * horizontalInput * time.deltaTime * speed);
+            /* transform.Translate moves object left/right 
+            time.deltaTime keeps it constant regardless of GPU by connecting
+            it to time(seconds)*/
+
+    }
+}
