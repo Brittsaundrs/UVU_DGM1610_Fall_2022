941ea74096301fd55ccd773dddc98e5f19d29b9d
brittsaundrs <10976300@uvu.edu>
2022-11-27 23:19:23 -0700

    Unit 6b -- Random UFOs at Random Locations
    Spawns random UFOs at random locations, still depends on hitting S key

--- UFO Defence Force Game/Assets/Scripts/EnemySpawnManager.cs
+++ UFO Defence Force Game/Assets/Scripts/EnemySpawnManager.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class EnemySpawnManager : MonoBehaviour
 {
     public GameObject[] ufoPrefabs; // Array to store UFO ships
-    public int ufoIndex;
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 20.0f;
+    
     
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.S))
-        Instantiate(ufoPrefabs[ufoIndex], new Vector3(0, 0, 15), ufoPrefabs[ufoIndex].transform.rotation);
+        {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int ufoIndex = Random.Range(0, ufoPrefabs.Length); // Picks random UFO from array
+        Instantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation); // Spawns indexed UFO from array at random location on x axis
+        }
     }
 }
