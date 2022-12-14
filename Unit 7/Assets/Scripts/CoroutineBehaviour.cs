5dbfdd54ce285ab99f33d4ae5eff8f918ae80b4e
brittsaundrs <10976300@uvu.edu>
2022-12-13 18:48:54 -0700

    Unit 7b -- Coroutines 03
    Videos:
    - 05 123 Go! Part 3
    - 06 Modifying the Coroutine for General Use

--- Unit 7/Assets/Scripts/CoroutineBehaviour.cs
+++ Unit 7/Assets/Scripts/CoroutineBehaviour.cs
@@ -2,31 +2,55 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
-[CreateAssetMenu]
 public class CoroutineBehaviour : MonoBehaviour
 /* Note: Coroutines can only live inside a MonoBehaviour, cannot
 be put into a ScriptableObject parent*/
 {
-    public UnityEvent startEvent, repeatEvent, endEvent;
+    public UnityEvent startEvent, startCountEvent, repeatCountEvent, endCountEvent, repeatUntilFalseEvent;
 
+    public bool canRun;
     public IntData counterNum;
     public float seconds = 3.0f;
     private WaitForSeconds wfsObj;
     private WaitForFixedUpdate wffuObj;
     // These will help keep RAM storage free
     // Start is called before the first frame update
-    IEnumerator Start()
+    
+    private void Start()
+    {
+        startEvent.Invoke();
+    }
+    public void StartCounting()
+    {
+        StartCoroutine(Counting());
+    }
+    private IEnumerator Counting()
     {
         wfsObj = new WaitForSeconds(seconds);
         wffuObj = new WaitForFixedUpdate();
-        startEvent.Invoke();
+        startCountEvent.Invoke();
         yield return wfsObj;
         while (counterNum.value > 0)
         {
-            repeatEvent.Invoke();
+            repeatCountEvent.Invoke();
             counterNum.value--; // counterNum now behaves as countdown
             yield return wfsObj;
         }
-        endEvent.Invoke();
+        endCountEvent.Invoke();
+    }
+
+    private IEnumerator RepeatUntilFalse()
+    {
+        while (canRun)
+        {
+            yield return wfsObj;
+            repeatUntilFalseEvent.Invoke();
+        }
+    }
+
+    public void StartRepeatUntilFalse()
+    {
+        canRun = true;
+        StartCoroutine(RepeatUntilFalse());
     }
 }
