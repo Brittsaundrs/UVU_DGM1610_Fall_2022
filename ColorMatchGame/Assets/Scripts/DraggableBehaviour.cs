8815a016d9e24265e7ff3233e7639a37a500aad2
brittsaundrs <10976300@uvu.edu>
2022-12-16 04:42:10 -0700

    Unit 7b -- Challenge 03
    Draggable Behaviour script now feeds mouse position to Circle obj. Still not tracking very well.

--- ColorMatchGame/Assets/Scripts/DraggableBehaviour.cs
+++ ColorMatchGame/Assets/Scripts/DraggableBehaviour.cs
@@ -6,26 +6,32 @@ public class DraggableBehaviour : MonoBehaviour
 {
     private Camera cameraObj;
     public bool draggable;
+    public Vector3 position; // gets world space
     
     // Start is called before the first frame update
     void Start()
     {
-        cameraObj = Camera.main;
+        cameraObj = Camera.main; // refer to camera info
     }
 
     public IEnumerator OnMouseDown()
     {
-        draggable = true;
+        draggable = true; // set draggable to true
 
-        while (draggable)
+        while (draggable) // while it's true...
         {
-            yield return new WaitForFixedUpdate();
-            Debug.Log("Drag");
+            yield return new WaitForFixedUpdate(); // give it a sec
+
+            // changes Vector3 position
+            position = cameraObj.ScreenToViewportPoint(Input.mousePosition);
+            transform.position = position; // feeds info to transform value
         }
     }
 
     private void OnMouseUp()
     {
-        draggable = false;
+        // stop changing position when mouse isn't on obj
+        // also stops OnMouseDown overall
+        draggable = false; 
     }
 }
